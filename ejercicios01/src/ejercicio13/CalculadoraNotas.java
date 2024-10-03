c67ca94005d6b78714a53bc3d8fc902fb692c64d
marta <msanchezl@iesrafaelalberti.es>
2024-10-03 19:45:38 +0200

    ejercicios 1

---
+++ ejercicios01/src/ejercicio13/CalculadoraNotas.java
@@ -0,0 +1,8 @@
+package ejercicio13;
+
+public class CalculadoraNotas {
+
+    public double segundaNota(float primeraNota, float notaDeseada) {
+        return (notaDeseada - (primeraNota * 0.4)) / 0.6;
+    }
+}
